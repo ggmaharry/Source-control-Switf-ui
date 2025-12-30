3d1fff266e03fa62b08b335b171056dced91c906
ggmaharry <ggmaharry@gmail.com>
2025-12-30 12:20:51 -0800

    first commit

--- sourceControlSwitfUI/ContentView.swift
+++ sourceControlSwitfUI/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            Circle()
+                .frame(width: 100, height: 100)
         }
         .padding()
+        
     }
 }
 
