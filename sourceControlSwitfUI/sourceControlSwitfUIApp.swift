71df75a0d85b8e610b11edb1216cf01612af3625
ggmaharry <ggmaharry@gmail.com>
2025-12-30 12:17:14 -0800

    Initial Commit

---
+++ sourceControlSwitfUI/sourceControlSwitfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourceControlSwitfUIApp.swift
+//  sourceControlSwitfUI
+//
+//  Created by protrek on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct sourceControlSwitfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
