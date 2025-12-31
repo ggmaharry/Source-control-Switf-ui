233945886548943aae6e03c6107baa350ca999e6
ggmaharry <ggmaharry@gmail.com>
2025-12-30 20:55:39 -0800

    add gitTest.swift

---
+++ sourceControlSwitfUI/gitTest.swift
@@ -0,0 +1,23 @@
+//
+//  gitTest.swift
+//  sourceControlSwitfUI
+//
+//  Created by protrek on 12/30/25.
+//
+
+import SwiftUI
+
+struct gitTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            
+        }, label: {
+            Text("Git Test")
+        })
+    }
+}
+
+#Preview {
+    gitTest()
+}
